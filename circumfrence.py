3d131e13b39470b7c8d653ba6d6c99bf495e37d7
rutba333 <sa2497589@gmail.com>
2024-10-03 21:39:08 +0600

    hello

---
+++ circumfrence.py
@@ -0,0 +1,12 @@
+
+def circle(radius):
+    return 2*3.1416*radius
+
+radius=float(input("Please enter the radius of the circle"))
+circumfrence=circle(radius)
+
+print("The circumfrence of the circle is:",circumfrence)
+
+  
+   
+    
